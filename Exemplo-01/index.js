ef01fd9e8f9274e8a2be82f4d81a773092674c33
Franciane Lark <franciane.lark019@gmail.com>
2023-03-21 13:02:05 -0400

    first commit

---
+++ Exemplo-01/index.js
@@ -0,0 +1,9 @@
+//Cálculo área de um triângulo
+let calculo, base, altura;
+
+base = prompt("Digite a base:");
+altura = prompt("Digite a altura:");
+
+calculo = (base*altura)/2;
+
+console.log("A área do Triângulo é:", calculo);
---
+++ Exemplo-02/index.js
@@ -0,0 +1,10 @@
+//Soma de dois números
+let numero1, numero2, soma;
+
+numero1 = prompt("Digite o número 1:");
+
+numero2 = prompt("Digite o número 2:");
+
+soma = parseInt(numero1) + parseInt(numero2);
+
+console.log(soma);

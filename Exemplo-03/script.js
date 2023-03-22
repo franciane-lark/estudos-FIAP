9c32b92c44e57c190f53759bc27db1c405a42623
Franciane Lark <franciane.lark019@gmail.com>
2023-03-22 11:45:12 -0400

    teste soma

---
+++ Exemplo-03/script.js
@@ -0,0 +1,8 @@
+let numero01,numero02, soma;
+
+numero01 = prompt("Digite o número 1: ");
+numero02 = prompt("Digite o número 2: ");
+
+soma = parseInt(numero01) + parseInt(numero02);
+
+console.log("A soma dos dois números é: ", soma);
